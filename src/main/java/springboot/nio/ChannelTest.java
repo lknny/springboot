869e9b02c4bcf49489e624567c20a48e8a87fe3a
Kun Liu <lknny@163.com>
2018-05-13 16:01:16 +0800

    uodate code

---
+++ src/main/java/springboot/nio/ChannelTest.java
@@ -0,0 +1,39 @@
+package springboot.nio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by ${ lknny@163.com } on 2018/5/13.
+ */
+public class ChannelTest {
+	private static final int port = 8123;
+
+	public static void main(String[] args) {
+		try {
+			//打开SocketChannel
+			SocketChannel sc = SocketChannel.open( );
+			sc.connect (new InetSocketAddress("somehost", port));
+
+			//打开ServerSocketChannel
+			ServerSocketChannel ssc = ServerSocketChannel.open( );
+			ssc.socket( ).bind (new InetSocketAddress (port));
+
+			DatagramChannel dc = DatagramChannel.open( );
+
+			//FileChannel只能通过 RandomAccessFile、FileInputStream 或 FileOutputStream 对象上调用 getChannel( )方法来获取
+			RandomAccessFile raf = new RandomAccessFile ("somefile", "r");
+			FileChannel fc = raf.getChannel( );
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/main/java/springboot/nio/ChannelTest2.java
@@ -0,0 +1,51 @@
+package springboot.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * Created by ${ lknny@163.com } on 2018/5/13.
+ */
+public class ChannelTest2 {
+
+	public static void main(String[] args) throws IOException {
+
+		//一个读通道，一个写通道
+		ReadableByteChannel source = Channels.newChannel(System.in);
+		WritableByteChannel dest = Channels.newChannel(System.out);
+
+		channelCopy(source,dest);
+
+		source.close();
+		dest.close();
+	}
+
+	private static void channelCopy(ReadableByteChannel source, WritableByteChannel dest) throws IOException{
+
+		ByteBuffer byteBuffer = ByteBuffer.allocate(16 * 1024);
+		ByteBuffer flag = ByteBuffer.allocate(4);
+		while (source.read(byteBuffer) != -1) {
+			byteBuffer.flip();
+			//输出标记
+			flag.put((byte)'-').put((byte)'-').put((byte)'-').put((byte) '>');
+			flag.flip();
+			dest.write(flag);
+			dest.write(byteBuffer);
+			flag.clear();
+
+			byteBuffer.compact();
+		}
+		byteBuffer.flip();
+		//确保缓冲区排干净
+		while (byteBuffer.hasRemaining()) {
+			flag.putChar('-').putChar('-').putChar('-');
+			flag.flip();
+			dest.write(byteBuffer);
+			flag.clear();
+		}
+
+	}
+}
