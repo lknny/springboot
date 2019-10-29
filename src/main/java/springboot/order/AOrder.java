bd331560a63aa0f77dc5d025bd59cdbc965fcff2
Kun Liu <lknny@163.com>
2019-10-29 08:53:25 +0800

    update

--- src/main/java/springboot/order/AOrder.java
+++ src/main/java/springboot/order/AOrder.java
@@ -1,6 +1,8 @@
 package springboot.order;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -9,7 +11,7 @@ import org.springframework.core.annotation.Order;
  * Created by ${ lknny@163.com } on 2019/10/26.
  */
 @Configuration
-public class AOrder implements InitializingBean {
+public class AOrder implements InitializingBean ,BeanPostProcessor{
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		System.out.println("this is--------------> third bean!");
@@ -18,5 +20,19 @@ public class AOrder implements InitializingBean {
 	@Order(3)
 	public AOrder get(){
 		return new AOrder();
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+		return o;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+		if (s.contains("order")) {
+			System.out.println(s);
+			System.out.println("bean post processor....");
+		}
+		return o;
 	}
 }
